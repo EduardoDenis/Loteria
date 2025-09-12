51262a77cf906ad612083e6b943f96a1e9514bdd
Eduardo Denis <edu.denisandrade@outlook.com>
2025-09-12 00:09:38 -0300

    Implementacao scrollState

--- app/src/main/java/com/eduardodenis/loteria/MainActivity.kt
+++ app/src/main/java/com/eduardodenis/loteria/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.eduardodenis.loteria
 
-import android.R.attr.onClick
-import android.app.Activity
 import android.os.Bundle
-import android.service.autofill.OnClickAction
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,45 +11,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.luminance
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.text.isDigitsOnly
-import androidx.core.view.WindowCompat
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -62,7 +44,8 @@ import com.eduardodenis.loteria.component.LoItemType
 import com.eduardodenis.loteria.component.LoNumberTextField
 import com.eduardodenis.loteria.ui.theme.DarkGreen
 import com.eduardodenis.loteria.ui.theme.LoteriaTheme
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -150,19 +133,29 @@ fun LotteryItem(name: String, onClick: () -> Unit) {
 
 @Composable
 fun FormScreen() {
+    val errorBets = stringResource(id = R.string.error_bets)
+    val errorNumbers = stringResource(id = R.string.error_numbers)
+    var qtdNumbers by remember { mutableStateOf("") }
+    var qtdBets by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+    val snackbarHostState by remember { mutableStateOf(SnackbarHostState()) }
+    val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val scrollState = rememberScrollState()
     Scaffold(
         modifier = Modifier
             .fillMaxSize(),
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = MaterialTheme.colorScheme.background,
+        snackbarHost = { SnackbarHost(snackbarHostState) }
     ) { innerPadding ->
 
-        var qtdNumbers by remember { mutableStateOf("") }
-        var qtdBets by remember { mutableStateOf("") }
+
         Column(
             verticalArrangement = Arrangement.spacedBy(20.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
                 .padding(innerPadding)
+                .verticalScroll(scrollState)
         ) {
             LoItemType(
                 name = "Mega Sena",
@@ -196,19 +189,55 @@ fun FormScreen() {
                 imeAction = ImeAction.Done
             ) {
                 if (it.length < 3) {
-                    qtdNumbers = validadeInput(it)
+                    qtdBets = validadeInput(it)
                 }
             }
 
             OutlinedButton(
+                enabled = qtdNumbers.isNotEmpty() && qtdBets.isNotEmpty(),
                 onClick = {
-
+                    val bets = qtdBets.toInt()
+                    val numbers = qtdNumbers.toInt()
+                    if (bets < 1 || bets > 10) {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(errorBets)
+                        }
+                    } else if (numbers < 6 || numbers > 15) {
+                        scope.launch {
+                            snackbarHostState.showSnackbar(errorNumbers)
+                        }
+                    } else {
+                        result = ""
+                        for (i in 1..bets) {
+                            result += "[$i]  "
+                            result += numberGenerator(numbers)
+                            result += "\n\n"
+                        }
+                    }
+                    keyboardController?.hide()
                 }
-            ) { Text(stringResource(id = R.string.bets_generate)) }
+            ) {
+                Text(stringResource(id = R.string.bets_generate))
+            }
 
+            Text(text = result)
 
         }
     }
+}
+
+private fun numberGenerator(qtd: Int): String {
+    val numbers = mutableSetOf<Int>()
+
+    while (true) {
+        val n = Random.nextInt(60)
+        numbers.add(n + 1)
+        if (numbers.size == qtd) {
+            break
+        }
+    }
+
+    return numbers.joinToString(" - ")
 }
 
 private fun validadeInput(input: String): String {
